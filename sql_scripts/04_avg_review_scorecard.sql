807f2985caa6dfbdb8804c758e91dd28aae4cf56
Agustianto <60026304+agustianto-lab@users.noreply.github.com>
2026-09-02 08:26:13 +0700

    Delete sql_scripts directory

--- sql_scripts/04_avg_review_scorecard.sql
+++
@@ -1,9 +0,0 @@
--- Query di bawah ini merupakan logika perhitungan scorecard avg_review yang ada pada looker.
--- perhitungan rata-rata untuk avg_review sebenarnya menggunakan tabel dataset orders_reviews langsung tanpa pembuatan view / query terlebih dahulu seperti grafik, chart atau scorecard yang lain
-
-SELECT 
-  ROUND(AVG(review_score), 2) AS average_review_score
-FROM 
-  `data-brazil-ecommerce.dataset_ujicoba.orders_reviews_dataset`
-WHERE 
-  review_score IS NOT NULL;
